283855e90a1ec89f1e79b6150ee4487544c6755f
sorious77 <49060014+sorious77@users.noreply.github.com>
2021-05-16 16:00:24 +0900

    Update auth.js

--- client/src/Component/auth.js
+++ client/src/Component/auth.js
@@ -1,7 +1,6 @@
 const users = [
     {ID : "wjddn", password:"123", name:"jeongwoo"},
     {ID : "yun", password:"123", name:"yunhyeong"},
-    {ID : "wjddn122087", password:"kmjmkm725@", name:"Lee JeongWoo"}
 ]
 
 export function signIn({ID, password}){
